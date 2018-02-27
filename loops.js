ec3bbb015c5eccbc49aaa5d95b9771d81ae0b4cb
Owen Rasar <stephanierasar@yahoo.com>
2018-02-27 22:11:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,21 @@
-var gifts = ['teddy bear', 'drone', 'doll']
- 
-function wrapGift(gift) {
-  console.log(`Wrapped ${gift} and added a bow!`)
+function forLoop(array) {
+for (let i = 0; i < 25; i++) {
+  if(i == 1) {
+    array.push('I am 1 strange loop.')
+    
+    } else {
+    array.push('I am ${i} strange loops.')
+    }
+  }
+  return array;
 }
 
-for (var i = 1; i < 100; i++) {
-  console.log( "Hello World the " + i + " time" );
+
+function whileLoop(i) {
+  let countdown = i;
+ 
+while (countdown > 0) {
+  console.log(--countdown)
+}
+return 'done';
 }
